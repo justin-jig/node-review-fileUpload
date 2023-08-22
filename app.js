ba45988095834f0c38477488e32e206221b21ea0
justin-jig <justin-j@naver.com>
2023-08-22 20:34:03 +0900

    update

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
-
+const aws = require('aws-sdk'); // aws 설정을 위한 모듈
+const multerS3 = require('multer-s3'); // aws s3에 업로드하기 위한 multer설정
 const app = express();
 const PORT = 8000;
 
@@ -16,19 +17,46 @@ app.use(express.json());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
 //multer 설정
-const storage = multer.diskStorage({
-    destination : (req,file,cb) => {
-        cb(null, './uploads/');
-    },
-    filename : (req, file, cb) => {
-        //파일 확장자 추출
-        const ext = path.extname(file.originalname);
-        // 파일 이름 추출
-        const newName = path.basename(file.originalname, ext) + Date.now() + ext;
-        cb(null, newName);
-    }
+// const storage = multer.diskStorage({
+//     destination : (req,file,cb) => {
+//         cb(null, './uploads/');
+//     },
+//     filename : (req, file, cb) => {
+//         //파일 확장자 추출
+//         const ext = path.extname(file.originalname);
+//         // 파일 이름 추출
+//         const newName = path.basename(file.originalname, ext) + Date.now() + ext;
+//         cb(null, newName);
+//     }
+// })
+// const upload = multer({storage});
+
+
+// aws 설정
+aws.config.update({
+    accessKeyId : 'AKIA6GQIYR7AT3DGOSF6',
+    secretAccessKey : "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",
+    region: 'ap-northeast-2'
 })
-const upload = multer({storage});
+// aws s3 인스턴스 생성
+const s3 = new aws.S3();
+// multer 설정 - aws
+const upload = multer({
+    storage : multerS3({
+        s3: s3, // s3 인스턴스
+        bucket : 'kdt9-justin', // 버킷 name
+        acl : 'public', // 파일 접근권한 (public-read로 해야 업로드 파일 공개)
+        metadata : function(req,file, cb) {
+            cb(null, {fieldName : file.fieldName});
+        },
+
+        key: function (req, file, cb) {
+            cb(null, Date.now().toString() + '-' + file.originalname);
+        }
+    })
+})
+
+
 
 app.get('/' ,(req,res) => {
     res.render('index'); 
